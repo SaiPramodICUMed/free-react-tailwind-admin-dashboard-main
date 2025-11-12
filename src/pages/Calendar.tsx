8fb3e9d879540a2ccd935d25c9a0813901ac9970
naveenmedidha16 <naveen.medidha@navasoftware.com>
2025-11-12 19:05:32 +0530

    calender customised

--- src/pages/Calendar.tsx
+++ src/pages/Calendar.tsx
@@ -124,26 +124,26 @@ const Calendar: React.FC = () => {
       <div className="rounded-2xl border  border-gray-200 bg-white dark:border-gray-800 dark:bg-white/[0.03]">
         <div className="custom-calendar">
           <FullCalendar
-            ref={calendarRef}
-            plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
-            initialView="dayGridMonth"
-            headerToolbar={{
-              left: "prev,next addEventButton",
-              center: "title",
-              right: "dayGridMonth,timeGridWeek,timeGridDay",
-            }}
-            events={events}
-            selectable={true}
-            select={handleDateSelect}
-            eventClick={handleEventClick}
-            eventContent={renderEventContent}
-            customButtons={{
-              addEventButton: {
-                text: "Add Event +",
-                click: openModal,
-              },
-            }}
-          />
+  ref={calendarRef}
+  plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+  initialView="dayGridMonth"
+  headerToolbar={{
+    left: "prev,next",
+    center: "title",
+    right: "", // no week/day buttons
+  }}
+  events={events}
+  selectable={true}
+  select={handleDateSelect}
+  eventClick={handleEventClick}
+  eventContent={renderEventContent}
+  customButtons={{
+    addEventButton: {
+      text: "Add Event +",
+      click: openModal,
+    },
+  }}
+/>
         </div>
         <Modal
           isOpen={isOpen}
